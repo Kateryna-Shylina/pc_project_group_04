f0f3c6b7e9f9af5f6d0346aee652f17dac6d0328
JustZereff <tapxyh1445@gmail.com>
2024-02-17 20:39:21 +0200

    fix bug and upgrade

--- src/user_interface.py
+++ src/user_interface.py
@@ -11,6 +11,8 @@ GREEN = "\033[92m"
 YELLOW="\033[93m"
 RESET = "\033[0m"
 BLUE = "\033[94m"
+FLY_BLUE = "\033[38;5;117m"
+PURPURE = "\033[35m"
 
 
 def help_info():
@@ -23,44 +25,48 @@ def help_info():
             f'-{YELLOW} exit  {BLUE} - Exit from program.\n{RESET}')
 
 def help_record_contact():
-    print(f'    List of comand for "record_contacts":\n'
-            f'-{YELLOW} add {BLUE} - add contact to phone book\n'
-            f'-{YELLOW} all {BLUE} - show all records\n'
-            f'-{YELLOW} add tel {BLUE} - add phone number to record with Name\n'
-            f'-{YELLOW} delete {BLUE} - delete record from list\n'
-            f'-{YELLOW} find {BLUE} - search for records by part of a name or phone number\n'
-            f'-{YELLOW} help {BLUE} - help\n'
-            f'-{YELLOW} exit  {BLUE} - exit from program\n{RESET}')
+    print(f"    List of comand for 'record_contacts':\n"
+            f"-{YELLOW} 'add' {BLUE} - add contact to phone book\n"
+            f"-{YELLOW} 'all' {BLUE} - show all records\n"
+            f"-{YELLOW} 'add tel' {BLUE} - add phone number to record with Name\n"
+            f"-{YELLOW} 'delete' {BLUE} - delete record from list\n"
+            f"-{YELLOW} 'find' {BLUE} - search for records by part of a name or phone number\n"
+            f"-{YELLOW} 'help' {BLUE} - help\n"
+            f"-{YELLOW} 'main menu' of 'back' {BLUE} - help\n"
+            f"-{YELLOW} exit  {BLUE} - exit from program\n{RESET}")
 
 def help_record_note():
-    print(f'    List of comand for "record_contacts":\n'
-            f'-{YELLOW} add {BLUE} - add contact to phone book\n'
-            f'-{YELLOW} all {BLUE} - show all records\n'
-            f'-{YELLOW} add tel {BLUE} - add phone number to record with Name\n'
-            f'-{YELLOW} delete {BLUE} - delete record from list\n'
-            f'-{YELLOW} find {BLUE} - search for records by part of a name or phone number\n'
-            f'-{YELLOW} help {BLUE} - help\n'
-            f'-{YELLOW} exit  {BLUE} - exit from program\n{RESET}')
+    print(f"    List of comand for 'record_contacts':\n"
+            f"-{YELLOW} 'add' {BLUE} - add contact to phone book\n"
+            f"-{YELLOW} 'all' {BLUE} - show all records\n"
+            f"-{YELLOW} 'add tel' {BLUE} - add phone number to record with Name\n"
+            f"-{YELLOW} 'delete' {BLUE} - delete record from list\n"
+            f"-{YELLOW} 'find' {BLUE} - search for records by part of a name or phone number\n"
+            f"-{YELLOW} 'help' {BLUE} - help\n"
+            f"-{YELLOW} 'main menu' of 'back' {BLUE} - help\n"
+            f"-{YELLOW} exit  {BLUE} - exit from program\n{RESET}")
 
 def help_opening_a_text_document():
-    print(f'    List of comand for "record_contacts":\n'
-            f'-{YELLOW} add {BLUE} - add contact to phone book\n'
-            f'-{YELLOW} all {BLUE} - show all records\n'
-            f'-{YELLOW} add tel {BLUE} - add phone number to record with Name\n'
-            f'-{YELLOW} delete {BLUE} - delete record from list\n'
-            f'-{YELLOW} find {BLUE} - search for records by part of a name or phone number\n'
-            f'-{YELLOW} help {BLUE} - help\n'
-            f'-{YELLOW} exit  {BLUE} - exit from program\n{RESET}')
+    print(f"   {PURPURE}List of comand for 'record_contacts':\n"
+            f"-{YELLOW} 'add' {BLUE} - add contact to phone book\n"
+            f"-{YELLOW} 'all' {BLUE} - show all records\n"
+            f"-{YELLOW} 'add tel' {BLUE} - add phone number to record with Name\n"
+            f"-{YELLOW} 'delete' {BLUE} - delete record from list\n"
+            f"-{YELLOW} 'find' {BLUE} - search for records by part of a name or phone number\n"
+            f"-{YELLOW} 'help' {BLUE} - help\n"
+            f"-{YELLOW} 'main menu' of 'back' {BLUE} - help\n"
+            f"-{YELLOW} exit  {BLUE} - exit from program\n{RESET}")
 
 def help_interacting_with_applications():
-    print(f'    List of comand for "record_contacts":\n'
-            f'-{YELLOW} add {BLUE} - add contact to phone book\n'
-            f'-{YELLOW} all {BLUE} - show all records\n'
-            f'-{YELLOW} add tel {BLUE} - add phone number to record with Name\n'
-            f'-{YELLOW} delete {BLUE} - delete record from list\n'
-            f'-{YELLOW} find {BLUE} - search for records by part of a name or phone number\n'
-            f'-{YELLOW} help {BLUE} - help\n'
-            f'-{YELLOW} exit  {BLUE} - exit from program\n{RESET}')
+    print(f"    List of comand for 'record_contacts':\n"
+            f"-{YELLOW} 'add' {BLUE} - add contact to phone book\n"
+            f"-{YELLOW} 'all' {BLUE} - show all records\n"
+            f"-{YELLOW} 'add tel' {BLUE} - add phone number to record with Name\n"
+            f"-{YELLOW} 'delete' {BLUE} - delete record from list\n"
+            f"-{YELLOW} 'find' {BLUE} - search for records by part of a name or phone number\n"
+            f"-{YELLOW} 'help' {BLUE} - help\n"
+            f"-{YELLOW} 'main menu' of 'back' {BLUE} - help\n"
+            f"-{YELLOW} exit  {BLUE} - exit from program\n{RESET}")
 
 def start_bot():
     print(f'{YELLOW}Hello! I`m your personal assistant! To finish working please enter "exit". To view all commands please enter "Help"{RESET}')
@@ -73,78 +79,96 @@ def start_bot():
     program_status = True
 
     while program_status:
-        data = input(">").lower()
+        data = input(f"{PURPURE}> {RESET}").lower()
         
         if data == "record contact":
-            if data == "exit":
-                book.save_to_file(filename)
-                program_status = False
-            elif data == "help":
-                help_record_contact()
-            elif data == "add":
-                name = input("Enter name:")
-                if (name):
-                    record = address_book.Record(name)
-                    phone = input('Enter phone number (or press "Enter" to continue):')
-                    if phone:
-                        record.add_phone(phone)
-                    birthday = input('Enter date of birth [DD-MM-YYYY](or press "Enter" to continue):')
-                    if birthday:
-                        record.add_birthday(birthday)
-                    book.add_record(record)
-            elif data == "add tel":
-                name = input("Enter name:")
-                if (name):
-                    rec = book.find(name)
-                    if rec:
-                        phone = input("Enter phone number:")
-                        rec.add_phone(phone)
-                    else:
-                        print(f"The name {name} was not found in the address book")
+            while True:
+                input_data = input(f"{FLY_BLUE}[record contact] {RESET}| {GREEN}Enter command: {RESET}")
+                if input_data == "exit":
+                    book.save_to_file(filename)
+                    program_status = False
+                elif input_data == 'main menu' or input_data == 'back':
+                    book.save_to_file(filename)
+                    print(f"{YELLOW} You have returned to the main menu.")
+                    break
+                elif input_data == "help":
+                    help_record_contact()
+                elif input_data == "add":
+                    name = input(f"{GREEN}Enter name: {RESET}")
+                    if (name):
+                        record = address_book.Record(name)
+                        phone = input(f"{GREEN}Enter phone number (or press 'Enter' to continue): {RESET}")
+                        if phone:
+                            record.add_phone(phone)
+                        birthday = input(f"{GREEN}Enter date of birth [DD-MM-YYYY](or press 'Enter' to continue): {RESET}")
+                        if birthday:
+                            record.add_birthday(birthday)
+                        book.add_record(record)
+                elif input_data == "add tel":
+                    name = input(f"{GREEN}Enter name: {RESET}")
+                    if (name):
+                        rec = book.find(name)
+                        if rec:
+                            phone = input(f"{GREEN}Enter phone number: {RESET}")
+                            rec.add_phone(phone)
+                        else:
+                            print(f"{RED}The name {name} was not found in the address book")
 
-            elif data=="delete":
-                name = input("Enter name:")
-                if (name ):
-                    nam=book.delete(name)
-                    if nam:
-                        print(f"{nam} deleted from address book")
-                    else:
-                        print(f"The name {name} was not found in the address book")
-            elif data == "find":
-                find_contact=input("Enter text for searching (part of name or phone number):")
-                book.get_find(find_contact)
+                elif input_data=="delete":
+                    name = input(f"{GREEN}Enter name: {RESET}")
+                    if (name ):
+                        nam=book.delete(name)
+                        if nam:
+                            print(f"{nam} deleted from address book")
+                        else:
+                            print(f"{RED}The name {name} was not found in the address book")
+                elif input_data == "find":
+                    find_contact=input(f"{GREEN}Enter text for searching (part of name or phone number): {RESET}")
+                    book.get_find(find_contact)
 
-            elif data == "all":
-                n=-1
-                if len(book.data.items())>10:
-                    n=10
-                book.get_all(n)
-            else:
-                print(f'The command "{data}" not defined')
+                elif input_data == "all":
+                    n=-1
+                    if len(book.data.items())>10:
+                        n=10
+                    book.get_all(n)
+                else:
+                    print(f'The command "{data}" not defined')
                 
         elif data == 'record note':
-            name = input("Enter command:")
-            if data == "exit":
-                book.save_to_file(filename)
-                program_status = False
-            elif data == 'help':
-                help_record_note()
+            while True:
+                input_data = input(f"{FLY_BLUE}[record note] {RESET}| {GREEN}Enter command: {RESET}")
+                if input_data == "exit":
+                    book.save_to_file(filename)
+                    program_status = False
+                elif input_data == 'main menu' or input_data == 'back':
+                    print(f"{YELLOW} You have returned to the main menu.")
+                    break
+                elif input_data == 'help':
+                    help_record_note()
         
         elif data == 'opening a text document':
-            name = input("Enter command:")
-            if data == "exit":
-                book.save_to_file(filename)
-                program_status = False
-            elif data == 'help':
-                help_opening_a_text_document()
+            while True:
+                input_data = input(f"{FLY_BLUE}[opening a text document] {RESET}| {GREEN}Enter command: {RESET}")
+                if input_data == "exit":
+                    book.save_to_file(filename)
+                    program_status = False
+                elif input_data == 'main menu' or input_data == 'back':
+                    print(f"{YELLOW} You have returned to the main menu.")
+                    break
+                elif data == 'help':
+                    help_opening_a_text_document()
         
         elif data == 'interacting with applications':
-            name = input("Enter command:")
-            if data == "exit":
-                book.save_to_file(filename)
-                program_status = False
-            elif data == 'help':
-                help_interacting_with_applications()
+            while True:
+                input_data = input(f"{FLY_BLUE}[interacting with applications] {RESET}| {GREEN}Enter command: {RESET}")
+                if input_data == "exit":
+                    book.save_to_file(filename)
+                    program_status = False
+                elif input_data == 'main menu' or input_data == 'back':
+                    print(f"{YELLOW} You have returned to the main menu.")
+                    break
+                elif input_data == 'help':
+                    help_interacting_with_applications()
         
         elif data == "help":
                 help_info()
@@ -154,6 +178,6 @@ def start_bot():
                 program_status = False
         
         else:
-            print(f"{RED}Invalid command, please enter one of: 'record_contact', 'record_note', 'interacting_with_files', 'interacting_with_applications'.{RESET}")
+            print(f"{RED}Invalid command, please enter one of: 'record contact', 'record note', 'opening a text document', 'interacting with_applications'.{RESET}")
 
 start_bot()
